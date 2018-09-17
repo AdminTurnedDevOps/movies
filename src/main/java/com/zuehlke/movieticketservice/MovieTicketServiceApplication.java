22ddf2881a7f4730b0899034cbd61f3ccc64e7d5
ynnckth <yannickstreit@hotmail.com>
2018-09-17 09:35:24 +0200

    init project

---
+++ src/main/java/com/zuehlke/movieticketservice/MovieTicketServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zuehlke.movieticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketServiceApplication.class, args);
+	}
+}

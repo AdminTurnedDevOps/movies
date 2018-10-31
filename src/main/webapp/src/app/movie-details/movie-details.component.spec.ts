57f82213687fd173506af3c2f6a2e5fdf4f04d34
ynnckth <yannickstreit@hotmail.com>
2018-10-31 16:17:16 +0100

    test movies-list and movie-details components

--- src/main/webapp/src/app/movie-details/movie-details.component.spec.ts
+++ src/main/webapp/src/app/movie-details/movie-details.component.spec.ts
@@ -1,25 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { MovieDetailsComponent } from './movie-details.component';
 
+
 describe('MovieDetailsComponent', () => {
-  let component: MovieDetailsComponent;
-  let fixture: ComponentFixture<MovieDetailsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MovieDetailsComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MovieDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
-    expect(component).toBeTruthy();
+    TestBed.configureTestingModule({
+      providers: [MovieDetailsComponent]
+    });
+
+    const movieDetailsComponent = TestBed.get(MovieDetailsComponent);
+    expect(movieDetailsComponent).toBeTruthy();
   });
 });
--- src/main/webapp/src/app/movies-list/movies-list.component.spec.ts
+++ src/main/webapp/src/app/movies-list/movies-list.component.spec.ts
@@ -1,25 +1,63 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
+import {MoviesListComponent} from './movies-list.component';
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {MovieService} from "../movie.service";
+import {of} from "rxjs/index";
+import {Movie} from "../movie";
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
-import { MoviesListComponent } from './movies-list.component';
+
+const movie1: Movie = {id: 1, title: "Batman Begins", posterUrl: "https://batman", ratings: []};
+const movie2: Movie = {id: 2, title: "Ted", posterUrl: "https://ted", ratings: []};
+
+const movieServiceMock = {
+  loadMovies: () => {
+    return of([movie1, movie2]);
+  }
+};
 
 describe('MoviesListComponent', () => {
-  let component: MoviesListComponent;
-  let fixture: ComponentFixture<MoviesListComponent>;
 
-  beforeEach(async(() => {
+  it('should apply the search filter correctly', () => {
     TestBed.configureTestingModule({
-      declarations: [ MoviesListComponent ]
-    })
-    .compileComponents();
-  }));
+      providers: [MoviesListComponent, FormBuilder, {provide: MovieService, useValue: movieServiceMock}]
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MoviesListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    const moviesListComponent = TestBed.get(MoviesListComponent);
+    moviesListComponent.ngOnInit();
+    moviesListComponent.onSearch();
+
+    expect(moviesListComponent.displayedMovies.length).toBe(2);
+    expect(moviesListComponent.displayedMovies[0].title).toBe('Batman Begins');
+
+    moviesListComponent.searchForm.controls.movieTitle.value = 'te';
+    moviesListComponent.onSearch();
+
+    expect(moviesListComponent.displayedMovies.length).toBe(1);
+    expect(moviesListComponent.displayedMovies[0].title).toBe('Ted');
+
+    moviesListComponent.searchForm.controls.movieTitle.value = '';
+    moviesListComponent.onSearch();
+
+    expect(moviesListComponent.displayedMovies.length).toBe(2);
+    expect(moviesListComponent.displayedMovies[0].title).toBe('Batman Begins');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should write the movies into the DOM', () => {
+    TestBed.configureTestingModule({
+      providers: [{provide: MovieService, useValue: movieServiceMock}],
+      declarations: [MoviesListComponent],
+      imports: [ReactiveFormsModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
+
+    const fixture = TestBed.createComponent(MoviesListComponent);
+    fixture.detectChanges();
+
+    const movieEntries = fixture.debugElement.queryAll(By.css('.movie-header'));
+
+    expect(movieEntries.length).toBe(2);
+    expect(movieEntries[0].childNodes[0].nativeNode.textContent).toBe('Batman Begins');
   });
 });
